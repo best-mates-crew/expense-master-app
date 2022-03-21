40adcab11aa606cfc3a37d13397f345ff8aba07f
Vladimir <vladimir.ivinski89@gmail.com>
2022-03-22 00:31:40 +0300

    wrapper calss

--- force-app/main/default/lwc/emReport/emReport.js
+++ force-app/main/default/lwc/emReport/emReport.js
@@ -5,17 +5,18 @@ import getReports from "@salesforce/apex/EMReportController.gerReports";
     const columns = [
         { label: 'Report', fieldName: "reportName"},
         { label: 'Expense', fieldName: "expenseName"},
+        { label: 'Status', fieldName: "expenseStatus"}
     ];
 
 export default class EmReport extends LightningElement {
 
     data = [];
+    columns= columns;
 
     async connectedCallback() {
         this.data = await getReports();
         console.log('data: ', this.data) ;
     }
-    
 
 
 }
