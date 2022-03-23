8aa81171eac48de10d9f3fa1fb0fab30f25f0ed4
Vladimir <vladimir.ivinski89@gmail.com>
2022-03-23 20:05:13 +0300

    created message service

--- force-app/main/default/lwc/emAllReports/emAllReports.js
+++ force-app/main/default/lwc/emAllReports/emAllReports.js
@@ -1,33 +1,33 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from "lwc";
 import getReport from "@salesforce/apex/EMReportController.getReport";
 
+import { publish, MessageContext } from "lightning/messageService";
+import RECORD_SELECTED_CHANNEL from "@salesforce/messageChannel/Record_Selected__c";
+
 const columns = [
-    { label: 'Report', fieldName: "name"},
-    { label: 'Status', fieldName: "status"},
+	{ label: "Report", fieldName: "name" },
+	{ label: "Status", fieldName: "status" }
 ];
 
 export default class EmAllReports extends LightningElement {
+	data;
+	columns = columns;
+	selectedRows;
+	_expensesInfo;
 
-    data;
-    columns= columns;
-    selectedRows;
-    expenseName;
-    expenseStatus;
-    
+	@wire(MessageContext)
+	messageContext;
 
-    async connectedCallback() {
-        this.data = await getReport();
-        console.log('data: ', this.data) ;
-    }
+	async connectedCallback() {
+		this.data = await getReport();
+		console.log("data: ", this.data);
+	}
 
-    getSelectedRows(event){
-        const rows = event.detail.selectedRows;
-        this.selectedRows = JSON.parse(JSON.stringify(rows));
-        console.log('this.selectedRows:',JSON.parse(JSON.stringify(rows))); 
+	getSelectedRows(event) {
+		this.selectedRows = event.detail.selectedRows;
         
-        this.expenseName = this.selectedRows.map(e => e.expenses.map(el => " " + el.expenseName ));
-        this.expenseStatus = this.selectedRows.map(e => e.expenses.map(el => el.expenseStatus));
-        console.log('this.expenseName:', this.expenseName);
-    }
+        const payload = { reportsInformation: this.selectedRows };
+		publish(this.messageContext, RECORD_SELECTED_CHANNEL, payload);
+	}
 
 }
--- force-app/main/default/lwc/emExpenseReportInfo/emExpenseReportInfo.js
+++ force-app/main/default/lwc/emExpenseReportInfo/emExpenseReportInfo.js
@@ -1,3 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { subscribe, MessageContext } from 'lightning/messageService';
+import RECORD_SELECTED_CHANNEL from '@salesforce/messageChannel/Record_Selected__c';
 
-export default class EmExpenseReportInfo extends LightningElement {}
+export default class EmExpenseReportInfo extends LightningElement {
+    subscription = null;
+    _reportsInfo;
+
+    @wire(MessageContext)
+    messageContext;
+
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+    }
+
+    subscribeToMessageChannel() {
+        this.subscription = subscribe(
+            this.messageContext,
+            RECORD_SELECTED_CHANNEL,
+            (message) => this.handleMessage(message)
+            );
+    }
+
+    handleMessage(message) {
+        this._reportsInfo = message;
+        console.log('this.reportsInfo:', this.reportsInfo);
+        console.log('message:', message);
+    }
+
+
+}
+
+		// this._expensesInfo = this.selectedRows.map((e) => e.expenses.map((el) => " " + el.expenseName));
+		// console.log('this._expensesInfo:', this._expensesInfo);
