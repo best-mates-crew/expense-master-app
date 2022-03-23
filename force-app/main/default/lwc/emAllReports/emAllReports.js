a5cd778e63da89701f98d462cd4736e2ae7222bb
Vladimir <vladimir.ivinski89@gmail.com>
2022-03-23 16:56:52 +0300

    WIP added Selection rows in table

--- force-app/main/default/lwc/emAllReports/emAllReports.js
+++ force-app/main/default/lwc/emAllReports/emAllReports.js
@@ -8,11 +8,26 @@ const columns = [
 
 export default class EmAllReports extends LightningElement {
 
-    data = [];
+    data;
     columns= columns;
+    selectedRows;
+    expenseName;
+    expenseStatus;
+    
 
     async connectedCallback() {
         this.data = await getReport();
         console.log('data: ', this.data) ;
     }
+
+    getSelectedRows(event){
+        const rows = event.detail.selectedRows;
+        this.selectedRows = JSON.parse(JSON.stringify(rows));
+        console.log('this.selectedRows:',JSON.parse(JSON.stringify(rows))); 
+        
+        this.expenseName = this.selectedRows.map(e => e.expenses.map(el => " " + el.expenseName ));
+        this.expenseStatus = this.selectedRows.map(e => e.expenses.map(el => el.expenseStatus));
+        console.log('this.expenseName:', this.expenseName);
+    }
+
 }
