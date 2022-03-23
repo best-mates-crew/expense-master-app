c79168207574973ddddbe250eb9a103c3fb3c242
Vladimir <vladimir.ivinski89@gmail.com>
2022-03-23 12:43:05 +0300

    created lwc for expense info

--- force-app/main/default/lwc/emAllReports/emAllReports.js
+++ force-app/main/default/lwc/emAllReports/emAllReports.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
+import getReport from "@salesforce/apex/EMReportController.getReport";
 
-export default class EmAllReports extends LightningElement {}
+const columns = [
+    { label: 'Report', fieldName: "name"},
+    { label: 'Status', fieldName: "status"},
+];
+
+export default class EmAllReports extends LightningElement {
+
+    data = [];
+    columns= columns;
+
+    async connectedCallback() {
+        this.data = await getReport();
+        console.log('data: ', this.data) ;
+    }
+}
