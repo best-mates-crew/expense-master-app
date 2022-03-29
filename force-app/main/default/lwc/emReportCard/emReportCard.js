c690446a43669fa5b5d963adeea40353ff678c2e
Vladimir <vladimir.ivinski89@gmail.com>
2022-03-29 15:13:42 +0300

    upd

--- force-app/main/default/lwc/emReportCard/emReportCard.js
+++ force-app/main/default/lwc/emReportCard/emReportCard.js
@@ -3,8 +3,11 @@ import { LightningElement, api } from 'lwc';
 export default class EmReportCard extends LightningElement {
 
     @api reports;
-    
+
     connectedCallback(){
     }
 
+    reportDetailHandler(event){
+
+    }
 }
